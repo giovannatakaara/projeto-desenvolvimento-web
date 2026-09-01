4347bdbb4f7ceee5a8052c549397ef49290bb6cf
giovannatakaara <gkptakaara@minha.fag.edu.br>
2026-09-01 14:46:14 -0300

    Feito Atv 5

---
+++ AtividadeCinco/calculadora.js
@@ -0,0 +1,7 @@
+const valorConta = 100;
+const percentualGorjeta = 15;
+const valorGorjeta = valorConta * (percentualGorjeta/100);
+const valorTotal = valorConta + valorGorjeta;
+
+console.log("Valor da Conta: ", valorConta, " Gorjeta(15%): ", valorGorjeta, 
+    " Total a Pagar: ", valorTotal);
